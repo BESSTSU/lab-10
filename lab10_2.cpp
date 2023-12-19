e2a41974629468db55d6e2ec261f85474629597d
BESSTSU <152589209+BESSTSU@users.noreply.github.com>
2023-12-19 15:13:36 +0700

    lab10_2.cpp

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,13 +6,19 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
+	dest <<"-------------------- BOOM ---------------------\n";
+	string text;
+	while(getline(source,text))
+	{
+	cout <<text <<"\n";
+	}
+	dest <<"-------------------- HA!! ---------------------"; 
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
     source.close();
     dest.close();
 	return 0;
 }
+
